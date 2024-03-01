6b8532ff2eb1fb4073c6fa00dc81db6eea3f84ec
Carlos <co9852587@gmail.com>
2024-03-01 13:43:44 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("#formulario");
 const nameInput = document.querySelector("#name");
-const emaiInput = document.querySelector("#email");
+const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 const jobInput = document.querySelector("#job");
 const messageTextArea = document.querySelector("#message");
@@ -8,18 +8,65 @@ const messageTextArea = document.querySelector("#message");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-//verifica se os nomes estão vazios 
+//verifica se os campos estão preenchidos! 
 
     if(nameInput.value === ""){
      alert("Por favor, preencher o campo nome!");
      return;
     }
 
-    if(emaiInput.value === ""){
+    if(emailInput.value === "" || !isEmailValid(emailInput.value)){
         alert("Preencher o campo email corretamente");
         return;
     }
 
-    form.submit();
+    
 
+    if(!validatePassword(passwordInput.value,8)){
+        alert("A senha precisa ter no minimo 8 digitos");
+        return;
+
+    }
+
+    if(jobInput.value === ""){
+        alert("Selecione alguma opção no campo 'situation of jobs' ");
+        return;
+    }
+
+    if(messageTextArea.value ===""){
+        alert("Por gentileza preencher o campo de texto ");
+        return;
+    }
+
+
+    form.submit();
 });
+
+//função para validar o email 
+
+function isEmailValid(email){
+
+    //cria uma regex para validar o email 
+
+    const  emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if(emailRegex.test(email)){
+        return true;
+    }
+
+    return false;
+    
+}
+
+
+//função valida a senha 
+
+function validatePassword(password, minDigits){
+    if(password.length >= minDigits){
+        return true
+    }
+
+    return false
+}
